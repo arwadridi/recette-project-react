fd6ff6b813e9a3be56632ba7f3c6e3d1fb59ac4d
arwa dridi <arwadridi624@gmail.com>
2023-08-28 13:52:26 +0200

    mise en place du contenu

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import styles from "./Content.module.scss";
 
 function content() {
   return (
-    <div className="flex-fill b3">content</div>
+    <div className="flex-fill container p-20 ">
+      <h1 className="my-30"> Découvrir nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}><div className={styles.grid}>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      
+      </div>
+      </div></div>
+      
   )
 }
 
