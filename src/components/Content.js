c3eac4d0f2ce8c35efacb2040767e65b341c40e2
arwa dridi <arwadridi624@gmail.com>
2023-08-28 12:03:09 +0200

    mise en page de l'application

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function content() {
+  return (
+    <div className="flex-fill b3">content</div>
+  )
+}
+
+export default content
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div className="b2">footer</div>
+  )
+}
+
+export default footer
